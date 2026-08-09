cb018c11c0a1c2a27f5dc1daaddd0e9973004bc1
Ayesha Ali <56182013+sanaa-sys@users.noreply.github.com>
2026-08-09 19:32:52 +1000

    modift next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,15 @@
-import type { NextConfig } from 'next'
+import type { NextConfig } from 'next';
+import path from 'path';
 
 const nextConfig: NextConfig = {
-  images: {
-    unoptimized: true,
-  },
-}
+    images: {
+        unoptimized: true,
+    },
+    webpack: (config, { isServer }) => {
+        // Add @ alias for paths
+        config.resolve.alias['@'] = path.join(__dirname);
+        return config;
+    },
+};
 
-export default nextConfig
+export default nextConfig;
